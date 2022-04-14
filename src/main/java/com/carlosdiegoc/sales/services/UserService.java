cc78e1e7437c409a7435fd92e76276d9ba75f4d2
CarlosDiegoC <carlos_diegoh@hotmail.com>
2022-04-14 10:50:14 -0300

    Exception handling - findById

--- src/main/java/com/carlosdiegoc/sales/services/UserService.java
+++ src/main/java/com/carlosdiegoc/sales/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.carlosdiegoc.sales.entities.User;
 import com.carlosdiegoc.sales.repositories.UserRepository;
+import com.carlosdiegoc.sales.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/carlosdiegoc/sales/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.carlosdiegoc.sales.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+
+}
